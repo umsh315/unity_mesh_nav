88ea5feb5230333f03d5a276b5789689d2564686
Yifan Luo <luoyifan902008@126.com>
2025-06-17 15:08:56 +0800

    新增rviz的3d导航插件

---
+++ src/utilities/waypoint_rviz_plugin/src/waypoint_tool_3d.cpp
@@ -0,0 +1,91 @@
+#include <waypoint_tool.hpp>
+
+#include <string>
+
+#include <rviz_common/display_context.hpp>
+#include <rviz_common/logging.hpp>
+#include <rviz_common/properties/string_property.hpp>
+#include <rviz_common/properties/qos_profile_property.hpp>
+
+namespace waypoint_rviz_plugin
+{
+WaypointTool::WaypointTool()
+: dddmr_rviz_default_plugins::tools::PoseTool(), qos_profile_(5)
+{
+  shortcut_key_ = 'w';
+
+  topic_property_ = new rviz_common::properties::StringProperty("Topic", "waypoint", "The topic on which to publish navigation waypionts.",
+                                       getPropertyContainer(), SLOT(updateTopic()), this);
+  
+  qos_profile_property_ = new rviz_common::properties::QosProfileProperty(
+    topic_property_, qos_profile_);
+}
+
+WaypointTool::~WaypointTool() = default;
+
+void WaypointTool::onInitialize()
+{
+  dddmr_rviz_default_plugins::tools::PoseTool::onInitialize();
+  qos_profile_property_->initialize(
+    [this](rclcpp::QoS profile) {this->qos_profile_ = profile;});
+  setName("3D Waypoint");
+  updateTopic();
+  vehicle_z = 0;        //初始时为0
+}
+
+void WaypointTool::updateTopic()
+{
+  rclcpp::Node::SharedPtr raw_node =
+    context_->getRosNodeAbstraction().lock()->get_raw_node();
+  sub_ = raw_node->template create_subscription<nav_msgs::msg::Odometry>("/state_estimation", 5 ,std::bind(&WaypointTool::odomHandler,this,std::placeholders::_1));
+  
+  pub_ = raw_node->template create_publisher<geometry_msgs::msg::PointStamped>("/way_point", qos_profile_);
+  pub_joy_ = raw_node->template create_publisher<sensor_msgs::msg::Joy>("/joy", qos_profile_);
+  clock_ = raw_node->get_clock();
+}
+
+
+void WaypointTool::onPoseSet(double x, double y, double z, double theta)
+{
+  sensor_msgs::msg::Joy joy;
+
+  joy.axes.push_back(0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(-1.0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(1.0);
+  joy.axes.push_back(1.0);
+  joy.axes.push_back(0);
+  joy.axes.push_back(0);
+
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(1);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+  joy.buttons.push_back(0);
+
+  joy.header.stamp = clock_->now();
+  joy.header.frame_id = "waypoint_tool";
+  pub_joy_->publish(joy);
+
+  geometry_msgs::msg::PointStamped waypoint;
+  waypoint.header.frame_id = "map";
+  waypoint.header.stamp = joy.header.stamp;
+  waypoint.point.x = x;
+  waypoint.point.y = y;
+  waypoint.point.z = z;       //航点的z轴限制位车辆高度
+
+  pub_->publish(waypoint);
+  usleep(10000);
+  pub_->publish(waypoint);
+}
+}
+
+#include <pluginlib/class_list_macros.hpp> 
+PLUGINLIB_EXPORT_CLASS(waypoint_rviz_plugin::WaypointTool, rviz_common::Tool)
